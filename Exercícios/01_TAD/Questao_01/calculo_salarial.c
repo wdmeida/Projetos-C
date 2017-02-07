a3488eca21cbbdc7fe12eb9234dc6e7b930d2832
Wagner Almeida <wdmeida@gmail.com>
2017-02-06 23:09:58 -0200

    Conclusão do exercício 01 da lista de TAD.

--- Exercícios/01_TAD/Questao_01/calculo_salarial.c
+++ Exercícios/01_TAD/Questao_01/calculo_salarial.c
@@ -8,31 +8,57 @@ void inicia_programa() {
    scanf("%d", &numero_funcionarios);
 
    if(numero_funcionarios > 0) calculo_bonificacao(numero_funcionarios);
-   else return;
 }//inicia_programa
 
 void calculo_bonificacao(int numero_funcionarios) {
+    //Aloca memória para as informações dos funcionários.
     DadosFuncionario *dados = (DadosFuncionario*) malloc(numero_funcionarios * sizeof(DadosFuncionario));
 
+    //Lê as informações dos funcionários.
+    le_informacoes(dados, numero_funcionarios);
+
+    //Imprime o resultado.
+    imprime(dados, numero_funcionarios);
+
+    //Libera a memória alocada para o usuário.
+    free(dados);
+}//calculo_bonificacao()
+
+
+int efetua_calculo(DadosFuncionario funcionario) {
+
+    //Calculo os anos trabalhados.
+    int valor_bonificacao = funcionario.tempo_servico * BONUS_ANO_SERVICO_PRESTADO;
+
+    //Verifica se o funcionário tem mais que 45 anos.
+    if(funcionario.idade > 45) valor_bonificacao += ((funcionario.idade - 45) * BONUS_ANO_SERVICO_PRESTADO_ACIMA_45);
+
+    //Calcula a bonificação pelo número de filhos.
+    if(funcionario.numero_filhos >= 3) valor_bonificacao += 45;
+    else valor_bonificacao += (funcionario.numero_filhos * BONUS_FILHO);
+
+    return valor_bonificacao;
+}//efetua_calculo()
+
+
+//Lé as informações dos funcionários para realizar os cálculos.
+void le_informacoes(DadosFuncionario* dados, int tamanho) {
     int i;
-    for (i = 0; i < numero_funcionarios; i++){
+
+    for (i = 0; i < tamanho; i++){
 
         scanf("%d %d %d %d", &dados[i].numero_inscricao,
                              &dados[i].idade,
                              &dados[i].tempo_servico,
                              &dados[i].numero_filhos);
     }
-
-    imprime(dados, numero_funcionarios);
-
-    free(dados);
-}//calculo_bonificacao()
+}//le_informacoes()
 
 void imprime(DadosFuncionario* dados, int tamanho) {
     int i;
 
     for (i = 0; i < tamanho; i++) {
-        printf("\nMatrícula: %d\nIdade: %d", dados[i].numero_inscricao, dados[i].idade);
+        printf("\nNumero de inscricao.: %d\nBonus...............: %d\n", dados[i].numero_inscricao, efetua_calculo(dados[i]));
     }
 }//imprime
 
--- Exercícios/01_TAD/Questao_01/calculo_salarial.h
+++ Exercícios/01_TAD/Questao_01/calculo_salarial.h
@@ -4,13 +4,15 @@
 /*
 
     Uma empresa pretende atribuir um Bônus de Natal aos seus empregados, em função da idade, do tempo de serviço
-(em anos) e do número de filhos. O Bônus é calculado da seguinte forma:
-• R$ 20,00 por ano de serviço prestado.
-• R$ 5,00 por cada ano de serviço, acima dos 45 anos de idade.
-• R$ 15,00 por filho, com teto de R$ 45,00.
-Escreva um programa que, a partir da leitura do número de inscrição, da idade, tempo de serviço e número de
-filhos de um empregado, calcule o valor do bônus correspondente. Você deve fazer um programa que receba dados
-numéricos e imprima o valor do bônus natalino.
+    (em anos) e do número de filhos. O Bônus é calculado da seguinte forma:
+
+        • R$ 20,00 por ano de serviço prestado.
+        • R$ 5,00 por cada ano de serviço, acima dos 45 anos de idade.
+        • R$ 15,00 por filho, com teto de R$ 45,00.
+
+    Escreva um programa que, a partir da leitura do número de inscrição, da idade, tempo de serviço e número de
+    filhos de um empregado, calcule o valor do bônus correspondente. Você deve fazer um programa que receba dados
+    numéricos e imprima o valor do bônus natalino.
 
 */
 
@@ -23,10 +25,12 @@ typedef struct{
     int idade;
     int tempo_servico;
     int numero_filhos;
+    int bonificacao;
 }DadosFuncionario;
 
 void inicia_programa();
 void calculo_bonificacao(int);
-void le_informacoes();
-void imprime(DadosFuncionario* dados, int tamanho);
+int efetua_calculo(DadosFuncionario);
+void le_informacoes(DadosFuncionario*, int);
+void imprime(DadosFuncionario*, int);
 #endif // CALCULO_SALARIAL_H_INCLUDED
